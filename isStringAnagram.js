cdb4e8e917aace4ef8c85e108c64bae3e2e81b6c
Tim Turner <timmyturner@me.com>
2018-02-10 17:03:21 -0800

    adds fibonacci iterative solution

---
+++ isStringAnagram.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// check to see if strings are anagrams
+
+// Solution 1 - time complexity O(nLogn) because of sort
+
+let isAnagram1 = (str, test) => {
+  str = str.toUpperCase()
+}
